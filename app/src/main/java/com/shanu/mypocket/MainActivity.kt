c7ec29c1e31106d93ba0b4a4f5e7a935b67ce153
shanu2409 <shanusingh2409@gmail>
2023-11-15 17:01:38 +0530

    first

---
+++ app/src/main/java/com/shanu/mypocket/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.shanu.mypocket
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // redirect to HomeActivity
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this, HomeActivity::class.java))
+            finish()
+        }, 4000)
+
+    }
+}
